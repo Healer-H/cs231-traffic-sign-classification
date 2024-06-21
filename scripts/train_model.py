75cd1f17c889d7dec388c806fd9f91c8db15fa6a
Minh Hieu Huynh <22520434@gm.uit.edu.vn>
2024-06-22 02:55:54 +0700

    bug: fix bug liên quan tới cấu hình max_depth

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -2,8 +2,6 @@ import os
 from src.config_reader import config
 from src.data_processing import load_annotations, extract_hog_features
 from src.model import (
-    create_knn_model,
-    train_model,
     save_model,
     knn_hyperparameter_tuning,
     random_forest_hyperparameter_tuning
@@ -17,25 +15,32 @@ def main():
 
     train_images_path = os.path.join(data_path, train_images_dir)
     train_labels_path = os.path.join(data_path, train_labels_dir)
-    
-    train_images, train_anotations = load_annotations(train_images_path, train_labels_path)
-    # Trích xuất HOG features từ tập train
+
+    train_images, train_anotations = load_annotations(
+        train_images_path, train_labels_path)
+
     resize_width, resize_height = 32, 64
     train_hog_features, train_labels = extract_hog_features(
         train_images, train_anotations, resize_width, resize_height)
-    
-    # Tìm kiếm siêu tham số tốt nhất và huấn luyện mô hình kNN
+
     best_knn = knn_hyperparameter_tuning(train_hog_features, train_labels)
+    print("Training the best kNN model...")
     best_knn.fit(train_hog_features, train_labels)
     save_model(best_knn, os.path.join(
-        config['output']['model_dir'], 'best_knn_model.pkl'))
+        config['output']['model_dir'],
+        config['output']['knn_model_dir'],
+        'best_knn_model.pkl')
+    )
 
-    # Fine-tuning và huấn luyện mô hình Random Forest
     best_rf = random_forest_hyperparameter_tuning(
         train_hog_features, train_labels)
+    print("Training the best Random Forest model...")
     best_rf.fit(train_hog_features, train_labels)
     save_model(best_rf, os.path.join(
-        config['output']['model_dir'], 'best_rf_model.pkl'))
+        config['output']['model_dir'],
+        config['output']['random_forest_model_dir'],
+        'best_rf_model.pkl')
+    )
 
 
 if __name__ == "__main__":
--- src/config_reader.py
+++ src/config_reader.py
@@ -4,6 +4,11 @@ import os
 def read_config(config_path='config/config.yaml'):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
+        if 'None' in config['model']['random_forest']['max_depth']:
+            config['model']['random_forest']['max_depth'].remove('None')
+            config['model']['random_forest']['max_depth'].append(None)
+            
+            
     return config
 
 config = read_config()
